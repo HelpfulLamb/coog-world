fa4e6f893c2ced0206c846798a8e5037828c2fe9
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-03-15 10:33:06 -0500

    updated backend connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv');
+const fs = require('fs')
 dotenv.config({ path: './.env' });
 
 const db = mysql.createPool({
@@ -7,7 +8,19 @@ const db = mysql.createPool({
     host: process.env.DB_host,
     user: process.env.DB_user,
     password: process.env.DB_password,
-    database: process.env.DB_database
+    database: process.env.DB_database,
+    ssl:{
+        ca: fs.readFileSync(process.env.SSL_CERT)
+    }
+});
+
+const query = 'SELECT * FROM rides';
+db.query(query, (err, result) => {
+    if(err){
+        console.error('Error: ', err);
+    } else {
+        console.log('Success', result);
+    }
 });
 
 module.exports = {
