2ff7914b85147f52f27d5db40bbb4ad4ff4b288a
eliza365 <ccelizabeth365@gmail.com>
2025-03-16 19:03:04 -0500

    trying to add shop changes

--- client/react-app/src/App.jsx
+++ client/react-app/src/App.jsx
@@ -5,6 +5,9 @@ import Home from './pages/Home.jsx';
 import Tickets from './pages/Buy-tickets.jsx';
 import Services from './pages/Dashboard.jsx';
 import Cart from './pages/Cart.jsx';
+import Shop from './pages/Shops.jsx';
+import Parkshops from './pages/shops/Parkshops.jsx';
+import Merchandise from './pages/shops/Merchandise.jsx';
 import Contact from './pages/Contact.jsx';
 import Register from './pages/registration/Register.jsx';
 import Footer from './pages/navigation/Footer.jsx';
@@ -20,8 +23,11 @@ function App() {
                     <Route path='/tickets' element={<Tickets />} />
                     <Route path='/services' element={<Services />} />
                     <Route path='/cart' element={<Cart />} />
+                    <Route path='/shop' element={<Shop />} />
                     <Route path='/contact' element={<Contact />} />
                     <Route path='/registration' element={<Register />} />
+                    <Route path='/parkshops' element={<Parkshops />} />
+                    <Route path='/merch' element={<Merchandise />} />
                 </Routes>
                 <Footer/>
             </div>
