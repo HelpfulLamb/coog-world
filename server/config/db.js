3eef3f3acd8c9a75df6813e810aa1b1f8567156b
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-03-24 11:49:39 -0500

    deployment ssl

--- server/config/db.js
+++ server/config/db.js
@@ -5,16 +5,24 @@ dotenv.config({ path: './.env' });
 
 
 // create connection to database
-const db = mysql.createPool({
+const dbConfig = {
     connectionLimit: process.env.DB_connectionLimit,
     host: process.env.DB_host,
     user: process.env.DB_user,
     password: process.env.DB_password,
     database: process.env.DB_database,
-    ssl:{
-       ca: fs.readFileSync(process.env.SSL_CERT)
-    }
-});
+    // ssl:{
+    //    ca: fs.readFileSync(process.env.SSL_CERT)
+    // }
+};
+
+if (process.env.USE_SSL === 'true') {
+    dbConfig.ssl = {
+      rejectUnauthorized: true // Or other Azure-recommended settings
+    };
+  }
+
+const db =mysql.createPool(dbConfig);
 
 // test database connection
 db.getConnection((err) => {
