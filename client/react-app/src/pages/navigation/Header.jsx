576cc8f13402970abd8dff20b0925dc555da0d2e
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-03-24 21:47:37 -0500

    header changes.

--- client/react-app/src/pages/navigation/Header.jsx
+++ client/react-app/src/pages/navigation/Header.jsx
@@ -1,7 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Logout } from '../registration/Login';
 import logo from '../../images/coogworldlogo.png';
 
 function Header(){
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const authStatus = localStorage.getItem('isAuthenticated');
+        if(authStatus === 'true'){
+            setIsAuthenticated(true);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        Logout(navigate);
+    };
+
     return(
         <nav className="nav-main">
             <div className="header-container">
@@ -15,11 +31,19 @@ function Header(){
                 </Link>
                 <ul className="header-list">
                     <li><Link to="/home" className="nav-link">Home</Link></li>
+                    <li><Link to="/about-us" className="nav-link">About Us</Link></li>
                     <li><Link to="/tickets" className="nav-link">Tickets</Link></li>
                     <li><Link to="/shop" className="nav-link">Shop</Link></li>
                     <li><Link to="/services" className="nav-link">Services</Link></li>
-                    <li><Link to="/contact" className="nav-link">Contact</Link></li>
-                    <li><Link to="/registration" className="nav-link">Register</Link></li>
+                    {isAuthenticated ? (
+                        <li>
+                            <button onClick={handleLogout} className='nav-link logout-button'>Logout</button>
+                        </li>
+                    ) : (
+                        <>
+                            <li><Link to='/login' className='nav-link'>Login</Link></li>
+                        </>
+                    )}
                 </ul>
             </div>
         </nav>
