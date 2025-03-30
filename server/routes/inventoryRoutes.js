564eeb319b41651787cbcafbdd1b4fdd789865c2
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-03-30 15:47:40 -0500

    new reports and updated reports.

--- server/routes/inventoryRoutes.js
+++ server/routes/inventoryRoutes.js
@@ -3,14 +3,15 @@ const express = require('express');
 const inventoryRouter = express.Router();
 
 /*Inventory Structure*/
-// create new rides
+// create new items
 inventoryRouter.post('/', inventoryController.createUnit);
 
-// retrieve rides (all or specific)
+// retrieve inventory (all or specific)
 inventoryRouter.get('/all', inventoryController.getAllInventory);
+inventoryRouter.get('/info', inventoryController.getInventoryInfo);
 inventoryRouter.get('/merchandise', inventoryController.getAllAvailableItems);
 
-// delete rides (all or specific)
+// delete items (all or specific)
 inventoryRouter.delete('/', inventoryController.deleteAllInventory);
 inventoryRouter.delete('/:id', inventoryController.deleteUnitById);
 
