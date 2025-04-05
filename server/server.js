85d39292cc3b942df04a91deb41bd42f3c45832d
HelpfulLamb <178034361+HelpfulLamb@users.noreply.github.com>
2025-04-05 17:29:31 -0500

    served files

--- server/server.js
+++ server/server.js
@@ -36,12 +36,11 @@ app.use('/api/kiosks', kioskRouter);
 app.use('/api/shop-purchases', shopRoutes);
 
 // Serve frontend files
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-    });
-}
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
 
 
 app.use((req, res) => {
