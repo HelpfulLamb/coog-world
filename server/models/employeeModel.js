9e3f202603ab61f7b128d7535213f6f436d1202b
skppnsk3 <bryanttruong0918@gmail.com>
2025-04-06 16:18:37 -0500

    Delete for employee

--- server/models/employeeModel.js
+++ server/models/employeeModel.js
@@ -34,5 +34,5 @@ exports.deleteAllEmployees = async () => {
 };
 
 exports.deleteEmployeeById = async (empid) => {
-    await db.query('DELETE FROM employees WHERE Emp_ID IN ?', [empid]);
+    await db.query('DELETE FROM employees WHERE Emp_ID = ?', [empid]);
 };
