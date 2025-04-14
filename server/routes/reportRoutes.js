1c23de960eea35d6e43a0cfaed2cc55849ebdcd7
Brennan Green <brennangreen9@gmail.com>
2025-04-13 23:41:05 -0500

    merch updates

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -6,6 +6,7 @@ reportRoutes.get('/rainouts', reportController.getRainoutsReport);
 reportRoutes.get('/rainout-rows', reportController.getRainoutRows);
 reportRoutes.get('/revenue', reportController.getRevenueReport);
 reportRoutes.get('/revenue-summary', reportController.getRevenueSummary);
+reportRoutes.get('/open-maintenance-count', reportController.getOpenMaintenanceCount);
 reportRoutes.get('/tickets-today', reportController.getTicketsSoldToday);
 reportRoutes.get('/revenue-details', reportController.getRevenueDetails);
 reportRoutes.get('/visitors-today', reportController.getTotalVisitorsToday);
